53a75913beb597656c33d6c957b370b07d32bfa1
Stéphane Lasmartres <SLasmartres@idt.pf>
2020-03-17 15:56:22 -1000

    route /analyze added

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,9 +1,24 @@
 import express = require('express');
+import bodyParser = require('body-parser');
+import { TextAnalyzer } from './text-analyzer';
 
 const app: express.Application = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
     res.send('Welcome!');
+});
+
+app.post('/analyze', (req, res) => {
+    const analyzer = new TextAnalyzer();
+
+    const text = req.body.text;
+
+    const result = analyzer.analyzeText(text);
+
+    res.json(result);
 });
 
 app.listen(3000, () => {
